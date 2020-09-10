4b887c80d2e5c41333d3fa96d36c72a765fbd85f
George Baronheid <baronheid.george@gmail.com>
2020-09-09 21:26:50 -0300

    Added ViewBinding

--- app/src/main/java/br/com/becare/MainActivity.kt
+++ app/src/main/java/br/com/becare/MainActivity.kt
@@ -2,10 +2,14 @@ package br.com.becare
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.com.becare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_main)
     }
 }
