b04bf0311fdb3cc6120ab2b82e2275ddeca427b3
George Baronheid <baronheid.george@gmail.com>
2020-09-17 21:35:43 -0300

    Finished landing and applied fragmentContainerView

--- app/src/main/java/br/com/becare/MainActivity.kt
+++ app/src/main/java/br/com/becare/MainActivity.kt
@@ -2,32 +2,22 @@ package br.com.becare
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
 import br.com.becare.databinding.ActivityMainBinding
-import br.com.becare.view.MainActivityAdapter
+import br.com.becare.view.LandingFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         this.supportActionBar!!.hide()
 
-        binding.activityMainViewPager.adapter = MainActivityAdapter(this)
 
-        binding.activityMainBottomNavView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.nav_menu_home -> binding.activityMainViewPager.setCurrentItem(0, true )
-                R.id.nav_menu_hospital -> binding.activityMainViewPager.setCurrentItem(1, true)
-            }
-            true
-        }
-        binding.activityMainViewPager.registerOnPageChangeCallback(object :
-        ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                binding.activityMainBottomNavView.menu.getItem(position).isChecked = true
-            }
-        })
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.activity_main_constraint, LandingFragment())
     }
 }
