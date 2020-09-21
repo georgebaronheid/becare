d401cf9279a82d358bf4771d0a4ddefcf74d22a8
George Baronheid <baronheid.george@gmail.com>
2020-09-21 15:20:29 -0300

    Removing from fragment

--- app/src/main/java/br/com/becare/view/LandingFragment.kt
+++ app/src/main/java/br/com/becare/view/LandingFragment.kt
@@ -6,13 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.becare.R
+import br.com.becare.databinding.LandingFragmentBinding
 
 class LandingFragment : Fragment() {
+
+    private lateinit var binding: LandingFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = LandingFragmentBinding.inflate(layoutInflater)
+
         return inflater.inflate(R.layout.landing_fragment, container, false)
     }
 
