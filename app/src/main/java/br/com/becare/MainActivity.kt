b6036924b778202b46c980c89637e649bb8a267d
George Baronheid <baronheid.george@gmail.com>
2020-09-21 23:16:22 -0300

    Not working

--- app/src/main/java/br/com/becare/MainActivity.kt
+++ app/src/main/java/br/com/becare/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.becare
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -9,6 +10,7 @@ import br.com.becare.databinding.ActivityMainBinding
 import br.com.becare.entities.Hospital
 import br.com.becare.entities.MedicalEntity
 import br.com.becare.service.RetrofitFactory
+import br.com.becare.view.DetailsActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -16,6 +18,9 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var detailtIntent: Intent
+    private var hospital: Hospital? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +30,10 @@ class MainActivity : AppCompatActivity() {
         this.supportActionBar!!.hide()
 
         retrofitTest()
-
+        detailtIntent = Intent(this, DetailsActivity::class.java).apply {
+            putExtra("HospitalEntity", hospital)
+        }
+        startActivity(detailtIntent)
     }
 
     private fun retrofitTest() {
@@ -34,13 +42,15 @@ class MainActivity : AppCompatActivity() {
         val call = RetrofitFactory().hospitalService().getBySearchKeyword(keyword)
 
         call.enqueue(object : Callback<Array<Hospital>> {
+
             override fun onResponse(
                 call: Call<Array<Hospital>>,
                 response: Response<Array<Hospital>>
             ) {
                 response.body()?.let {
-                    Log.i("Match for $keyword", it.get(0).toString())
-                    intent.putExtra("HospitalEntity", it)
+                    Log.i("Match for $keyword", it[0].toString())
+                    hospital = it[0]
+                    Toast.makeText(this@MainActivity, it[0].toString(), Toast.LENGTH_LONG).show()
                 } ?: Toast.makeText(this@MainActivity, "Hospital não localizado", Toast.LENGTH_LONG)
                     .show()
             }
@@ -49,7 +59,6 @@ class MainActivity : AppCompatActivity() {
                 Log.e("Erro:", t?.message!!)
             }
 
-        }
-        )
+        })
     }
 }
--- app/src/main/java/br/com/becare/view/DetailsActivity.kt
+++ app/src/main/java/br/com/becare/view/DetailsActivity.kt
@@ -1,24 +1,87 @@
 package br.com.becare.view
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import br.com.becare.R
 import br.com.becare.databinding.ActivityDetailsBinding
+import br.com.becare.entities.Hospital
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 class DetailsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityDetailsBinding
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         this.supportActionBar!!.hide()
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
-        TODO("Not yet implemented")
+    override fun onMapReady(p0: GoogleMap) {
+
+        map = p0
+
+        val hospitalObject = intent.getSerializableExtra("HospitalEntity") as Hospital
+
+        val location = LatLng(hospitalObject.latitude, hospitalObject.longitude)
+        map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title(hospitalObject.nome)
+                .snippet(hospitalObject.logradouro)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        )
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12.5F))
+
+        map.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
+            override fun getInfoWindow(p0: Marker?): View? {
+                return null
+            }
+
+            override fun getInfoContents(marker: Marker?): View {
+                val info = LinearLayout(applicationContext)
+                info.orientation = LinearLayout.VERTICAL
+
+                //--Título
+                val title = TextView(applicationContext)
+                title.setTextColor(Color.BLACK)
+                title.gravity = Gravity.START
+                title.setTypeface(null, Typeface.BOLD)
+                title.text = marker!!.title
+
+                //--Complemento
+                val snippet = TextView(applicationContext)
+                snippet.setTextColor(Color.GRAY)
+                snippet.text = marker.snippet
+
+                //--Adiciona o titulo e o complemento na marca
+                info.addView(title)
+                info.addView(snippet)
+
+                return info
+            }
+
+        })
     }
+
 }
